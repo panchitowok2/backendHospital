62bd0556bb53e00f13c017ecd3b267b67554eac7
Stefano <stefanofabi96@gmail.com>
2023-10-03 18:26:46 -0300

    Agregar transaccion de medicamentos mas recetados

--- controllers/controlador_medicamentos.js
+++ controllers/controlador_medicamentos.js
@@ -13,10 +13,90 @@ var controller = {
     where especialidad.id = $especialidadId and tratamiento_farmacologico.fecha between $fechaInicio and $fechaFinal 
     group by medicamento.id, medicamento.nombre
     */
-    buscarMedicamentosMasRecetados: (req, res) => {
+    buscarMedicamentosMasRecetados: async (req, res) => {
 
-      },
-     
+      const params = req.body
+
+      const fechaInicio = new Date(params.fecha_inicio)
+      const fechaFinal = new Date(params.fecha_final)
+      const especialidadId = new mongoose.Types.ObjectId(params.especialidad);
+
+      console.log(params)
+
+      const resultado = await TratamientoFarmacologico.aggregate([
+        {
+          $match: {
+            fecha_inicio: {
+              $gte: fechaInicio,
+              $lte: fechaFinal
+            }
+          }
+        }, 
+        {
+          $lookup: {
+            from: 'medico',
+            localField: 'medico',
+            foreignField: '_id',
+            as: 'medico'
+          }
+        },
+        {
+          $lookup: {
+            from: 'especialidad',
+            localField: 'medico.especialidades',
+            foreignField: '_id',
+            as: 'especialidad'
+          }
+        },
+        {
+          $unwind: "$especialidad"
+        },
+        {
+          $match: {
+            "especialidad._id": especialidadId
+          }
+        },
+        {
+          $lookup: {
+            from: 'dosificacion',
+            localField: 'dosificaciones',
+            foreignField: '_id',
+            as: 'dosificacion'
+          }
+        },
+        {
+          $unwind: "$dosificacion"
+        },
+        {
+          $lookup: {
+            from: 'medicamento',
+            localField: 'dosificacion.medicamento',
+            foreignField: '_id',
+            as: 'medicamento'
+          }
+        },
+        {
+          "$group": {
+            _id: "$medicamento",
+            count: {
+              $sum: 1
+            },
+            medicamento: {
+              $first: "$medicamento" // Conservar información sobre el medicamento
+            }
+          }
+        },
+        {
+          $project: {
+            _id: 0, // Elimina el campo _id de la salida
+            medicamento: 1, // Conserva el campo medicamento
+            count: 1 // Conserva el campo count
+          }
+        }
+      ])
+    
+      return res.status(200).send(resultado);
+    },  
 }
 
 export default controller
