6a34256637d9c9aeb3655ae70f9a9baa2db2e1d6
Stefano <stefanofabi96@gmail.com>
2023-10-03 22:09:10 -0300

    Añadir las verificaciones necesarias antes de llamar al controlador

--- controllers/controlador_medicamentos.js
+++ controllers/controlador_medicamentos.js
@@ -2,17 +2,23 @@ import mongoose from 'mongoose'
 import TratamientoFarmacologico from '../models/tratamiento_farmacologico.js'
 
 var controller = {
-    /* Funcion para buscar medicamentos mas recetados
+  /* Funcion para buscar medicamentos mas recetados
+  * 
+  *   select medicamento.id, medicamento.nombre, count(*)
+  *  from tratamiento_farmacologico
+  *  inner join medico on tratamiento_farmacologico.medico_id = medico .id
+  *  inner join especialidad on medico.especialidad_id = especialidad.id
+  *  inner join dosificacion on tratamiento_farmacologico.id = dosificacion.tratamiento_farmacologico_id
+  *  inner join medicamento on dosificacion.medicamento_id = medicamento.id
+  *  where especialidad.id = $especialidadId and tratamiento_farmacologico.fecha between $fechaInicio and $fechaFinal 
+  *  group by medicamento.id, medicamento.nombre
+  */
 
-    select medicamento.id, medicamento.nombre, count(*)
-    from tratamiento_farmacologico
-    inner join medico on tratamiento_farmacologico.medico_id = medico .id
-    inner join especialidad on medico.especialidad_id = especialidad.id
-    inner join dosificacion on tratamiento_farmacologico.id = dosificacion.tratamiento_farmacologico_id
-    inner join medicamento on dosificacion.medicamento_id = medicamento.id
-    where especialidad.id = $especialidadId and tratamiento_farmacologico.fecha between $fechaInicio and $fechaFinal 
-    group by medicamento.id, medicamento.nombre
-    */
+  /* 
+  * Precondiciones:
+  * especialidad es un objeto valido
+  * las fechas tienen formato valido
+  */
     buscarMedicamentosMasRecetados: async (req, res) => {
 
       const params = req.body
--- routes/ruta_medicamento.js
+++ routes/ruta_medicamento.js
@@ -1,9 +1,43 @@
 import express from 'express'
 import controlador_medicamentos from '../controllers/controlador_medicamentos.js'
+import Especialidad from '../models/especialidad.js'
 
 const router = express.Router()
 
+const validarRequestMedicamentos = (req, res, next) => {
+    // Comprueba si las propiedades 'from' y 'message' están presentes y no están vacías
+    if (!req.body.fecha_inicio || !req.body.fecha_final || !req.body.especialidad) {
+      return res.status(500).json({ error: 'Las propiedades "fecha_inicio", "fecha_final" y "especialidad" son requeridas' });
+    }
+
+    const fechaInicio = new Date(req.body.fecha_inicio);
+    const fechaFinal = new Date(req.body.fecha_final);
+
+    // Verifica si las fechas son válidas
+    if (isNaN(fechaInicio.getTime()) || isNaN(fechaFinal.getTime())) {
+        return res.status(500).json({ error: 'Las fechas proporcionadas no son válidas' });
+    }
+
+    // Aceptar la solicitud
+    next();
+};
+
+const verificarEspecialidad = async (req, res, next) => {
+    try {
+      const especialidad = await Especialidad.findOne({ _id: req.body.especialidad }).exec();
+      
+      if (!especialidad) {
+        return res.status(404).json({ mensaje: 'Especialidad no encontrada' });
+      }
+      console.log(especialidad)
+      // Aceptar la solicitud
+      next();
+    } catch (error) {
+      return res.status(500).json({ mensaje: 'Error interno del servidor. '+error });
+    }
+};
+
 // Ruta para obtener datos de persona
-router.get('/buscarMedicamentosMasRecetados', controlador_medicamentos.buscarMedicamentosMasRecetados)
+router.get('/buscarMedicamentosMasRecetados', validarRequestMedicamentos, verificarEspecialidad, controlador_medicamentos.buscarMedicamentosMasRecetados)
 
 export default router
