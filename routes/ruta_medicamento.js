79d2b975aaa003ca034ff3c27ee28dfa21dd7884
Stefano <stefanofabi96@gmail.com>
2023-11-05 20:25:12 -0300

    Agregar mensajes de error informativos cuando la consulta de medicamentos devuelve vacío

--- routes/ruta_medicamento.js
+++ routes/ruta_medicamento.js
@@ -1,13 +1,15 @@
 import express from 'express'
 import controlador_medicamentos from '../controllers/controlador_medicamentos.js'
 import Especialidad from '../models/especialidad.js'
+import TratamientoFarmacologico from '../models/tratamiento_farmacologico.js'
+import mongoose from 'mongoose'
 
 const router = express.Router()
 
 const validarRequestMedicamentos = (req, res, next) => {
-    // Comprueba si las propiedades 'from' y 'message' están presentes y no están vacías
+    // Comprueba si el usuario envio todos los parametros necesarios
     if (!req.body.fecha_inicio || !req.body.fecha_final || !req.body.especialidad) {
-      return res.status(500).json({ error: 'Las propiedades "fecha_inicio", "fecha_final" y "especialidad" son requeridas' });
+      return res.status(500).json({ message: 'Las propiedades "fecha_inicio", "fecha_final" y "especialidad" son requeridas' });
     }
 
     const fechaInicio = new Date(req.body.fecha_inicio);
@@ -15,7 +17,7 @@ const validarRequestMedicamentos = (req, res, next) => {
 
     // Verifica si las fechas son válidas
     if (isNaN(fechaInicio.getTime()) || isNaN(fechaFinal.getTime())) {
-        return res.status(500).json({ error: 'Las fechas proporcionadas no son válidas' });
+        return res.status(500).json({ message: 'Las fechas proporcionadas no son válidas' });
     }
 
     // Aceptar la solicitud
@@ -27,18 +29,182 @@ const verificarEspecialidad = async (req, res, next) => {
       const especialidad = await Especialidad.findOne({ _id: req.body.especialidad }).exec();
       
       if (!especialidad) {
-        return res.status(404).json({ mensaje: 'Especialidad no encontrada' });
+        return res.status(404).json({ message: 'Especialidad no encontrada' });
       }
      
       // Aceptar la solicitud
       next();
-    } catch (error) {
-      return res.status(500).json({ mensaje: 'Error interno del servidor. '+error });
+    } catch (err) {
+      return res.status(500).json({ 
+        message: 'Error interno del servidor',
+        errors: err.errors
+      });
     }
 };
 
+const existenTratamientosEnEsaFecha = async (req, res, next) => {
+  const params = req.body
+  const fechaInicio = new Date(params.fecha_inicio)
+  const fechaFinal = new Date(params.fecha_final)
+
+  try {
+    
+    const tratamientos = await TratamientoFarmacologico.aggregate([
+      {
+        $match: {
+          fecha_inicio: {
+            $gte: fechaInicio,
+            $lte: fechaFinal
+          }
+        }
+      },
+    ])
+
+    if (tratamientos.length === 0) {
+      return res.status(404).json({ message: 'No existen tratamientos para el rango de fechas seleccionado' });
+    }
+   
+    // Aceptar la solicitud
+    next();
+  } catch (err) {
+    return res.status(500).json({ 
+      message: 'Error interno del servidor',
+      errors: err.errors
+    });
+  }
+};
+
+const existenTratamientosRealizadosPorMedicosConEsaEspecialidad = async (req, res, next) => {
+  const params = req.body
+  const fechaInicio = new Date(params.fecha_inicio)
+  const fechaFinal = new Date(params.fecha_final)
+  const especialidadId = new mongoose.Types.ObjectId(params.especialidad);
+
+  try {
+    
+    const tratamientos = await TratamientoFarmacologico.aggregate([
+      {
+        $match: {
+          fecha_inicio: {
+            $gte: fechaInicio,
+            $lte: fechaFinal
+          }
+        }
+      },
+      {
+        $lookup: {
+          from: 'medico',
+          localField: 'medico',
+          foreignField: '_id',
+          as: 'medico'
+        }
+      },
+      {
+        $lookup: {
+          from: 'especialidad',
+          localField: 'medico.especialidades',
+          foreignField: '_id',
+          as: 'especialidad'
+        }
+      },
+      {
+        $unwind: "$especialidad"
+      },
+      {
+        $match: {
+          "especialidad._id": especialidadId
+        }
+      }
+    ])
+
+    if (tratamientos.length === 0) {
+      return res.status(404).json({ message: 'No existen tratamientos realizados por medicos con dicha especialidad en el rango de fechas seleccionado' });
+    }
+   
+    // Aceptar la solicitud
+    next();
+  } catch (err) {
+    return res.status(500).json({ 
+      message: 'Error interno del servidor',
+      errors: err.errors
+    });
+  }
+};
+
+const existenTratamientosEnEsaFechaConDosificaciones = async (req, res, next) => {
+  const params = req.body
+  const fechaInicio = new Date(params.fecha_inicio)
+  const fechaFinal = new Date(params.fecha_final)
+  const especialidadId = new mongoose.Types.ObjectId(params.especialidad);
+
+  try {
+    
+    const tratamientos = await TratamientoFarmacologico.aggregate([
+      {
+        $match: {
+          fecha_inicio: {
+            $gte: fechaInicio,
+            $lte: fechaFinal
+          }
+        }
+      },
+      {
+        $addFields: {
+          numeroDosificaciones: { $size: "$dosificaciones" }
+        }
+      },
+      {
+        $match: {
+          numeroDosificaciones: { $gt: 0 }
+        }
+      },
+      {
+        $lookup: {
+          from: 'medico',
+          localField: 'medico',
+          foreignField: '_id',
+          as: 'medico'
+        }
+      },
+      {
+        $lookup: {
+          from: 'especialidad',
+          localField: 'medico.especialidades',
+          foreignField: '_id',
+          as: 'especialidad'
+        }
+      },
+      {
+        $unwind: "$especialidad"
+      },
+      {
+        $match: {
+          "especialidad._id": especialidadId
+        }
+      }
+    ]);
+    
+
+    if (tratamientos.length === 0) {
+      return res.status(404).json({ message: 'No existen tratamientos con dosificaciones para el rango de fechas seleccionado' });
+    }
+
+    console.log(tratamientos)
+   
+    // Aceptar la solicitud
+    next();
+  } catch (err) {
+    return res.status(500).json({ 
+      message: 'Error interno del serviddor',
+      errors: err.errors
+    });
+  }
+};
+
+
+
 // Ruta para obtener datos de persona
 router.post('/obtenerMedicamentos', controlador_medicamentos.obtenerMedicamentos)
-router.get('/buscarMedicamentosMasRecetados', validarRequestMedicamentos, verificarEspecialidad, controlador_medicamentos.buscarMedicamentosMasRecetados)
+router.get('/buscarMedicamentosMasRecetados', validarRequestMedicamentos, verificarEspecialidad, existenTratamientosEnEsaFecha, existenTratamientosRealizadosPorMedicosConEsaEspecialidad, existenTratamientosEnEsaFechaConDosificaciones, controlador_medicamentos.buscarMedicamentosMasRecetados)
 
 export default router
