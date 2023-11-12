5e04ed5a976379d11ac39a669417259f34203113
Stefano <stefanofabi96@gmail.com>
2023-11-11 21:22:25 -0300

    Mover verificaciones a una carpeta de middlewares

---
+++ middlewares/middleware_medicamento.js
@@ -0,0 +1,206 @@
+import Especialidad from '../models/especialidad.js'
+import TratamientoFarmacologico from '../models/tratamiento_farmacologico.js'
+import mongoose from 'mongoose'
+
+const validarRequestMedicamentos = (req, res, next) => {
+    // Comprueba si el usuario envio todos los parametros necesarios
+    if (!req.body.fecha_inicio || !req.body.fecha_final || !req.body.especialidad) {
+      return res.status(400).json({ message: 'Las propiedades "fecha_inicio", "fecha_final" y "especialidad" son requeridas' });
+    }
+
+    const fechaInicio = new Date(req.body.fecha_inicio);
+    const fechaFinal = new Date(req.body.fecha_final);
+
+    // Verifica si las fechas son válidas
+    if (isNaN(fechaInicio.getTime()) || isNaN(fechaFinal.getTime())) {
+        return res.status(400).json({ message: 'Las fechas proporcionadas no son válidas' });
+    }
+
+    // Aceptar la solicitud
+    next();
+};
+
+const verificarEspecialidad = async (req, res, next) => {
+    try {
+      const especialidad = await Especialidad.findOne({ _id: req.body.especialidad }).exec();
+      
+      if (!especialidad) {
+        return res.status(404).json({ message: 'Especialidad no encontrada' });
+      }
+     
+      // Aceptar la solicitud
+      next();
+    } catch (err) {
+      return res.status(500).json({ 
+        message: 'Error interno del servidor',
+        errors: err.errors
+      });
+    }
+};
+
+const existenTratamientosEnEsaFecha = async (req, res, next) => {
+  const params = req.body
+  const fechaInicio = new Date(params.fecha_inicio)
+  const fechaFinal = new Date(params.fecha_final)
+
+  try {
+    
+    const tratamientos = await TratamientoFarmacologico.aggregate([
+      {
+        $match: {
+          fecha_inicio: {
+            $gte: fechaInicio,
+            $lte: fechaFinal
+          }
+        }
+      },
+    ])
+
+    if (tratamientos.length === 0) {
+      return res.status(404).json({ message: 'No existen tratamientos para el rango de fechas seleccionado' });
+    }
+   
+    // Aceptar la solicitud
+    next();
+  } catch (err) {
+    return res.status(500).json({ 
+      message: 'Error interno del servidor',
+      errors: err.errors
+    });
+  }
+};
+
+const existenTratamientosRealizadosPorMedicosConEsaEspecialidad = async (req, res, next) => {
+  const params = req.body
+  const fechaInicio = new Date(params.fecha_inicio)
+  const fechaFinal = new Date(params.fecha_final)
+  const especialidadId = new mongoose.Types.ObjectId(params.especialidad);
+
+  try {
+    
+    const tratamientos = await TratamientoFarmacologico.aggregate([
+      {
+        $match: {
+          fecha_inicio: {
+            $gte: fechaInicio,
+            $lte: fechaFinal
+          }
+        }
+      },
+      {
+        $lookup: {
+          from: 'medico',
+          localField: 'medico',
+          foreignField: '_id',
+          as: 'medico'
+        }
+      },
+      {
+        $lookup: {
+          from: 'especialidad',
+          localField: 'medico.especialidades',
+          foreignField: '_id',
+          as: 'especialidad'
+        }
+      },
+      {
+        $unwind: "$especialidad"
+      },
+      {
+        $match: {
+          "especialidad._id": especialidadId
+        }
+      }
+    ])
+
+    if (tratamientos.length === 0) {
+      return res.status(404).json({ message: 'No existen tratamientos realizados por medicos con dicha especialidad en el rango de fechas seleccionado' });
+    }
+   
+    // Aceptar la solicitud
+    next();
+  } catch (err) {
+    return res.status(500).json({ 
+      message: 'Error interno del servidor',
+      errors: err.errors
+    });
+  }
+};
+
+const existenTratamientosEnEsaFechaConDosificaciones = async (req, res, next) => {
+  const params = req.body
+  const fechaInicio = new Date(params.fecha_inicio)
+  const fechaFinal = new Date(params.fecha_final)
+  const especialidadId = new mongoose.Types.ObjectId(params.especialidad);
+
+  try {
+    
+    const tratamientos = await TratamientoFarmacologico.aggregate([
+      {
+        $match: {
+          fecha_inicio: {
+            $gte: fechaInicio,
+            $lte: fechaFinal
+          }
+        }
+      },
+      {
+        $addFields: {
+          numeroDosificaciones: { $size: "$dosificaciones" }
+        }
+      },
+      {
+        $match: {
+          numeroDosificaciones: { $gt: 0 }
+        }
+      },
+      {
+        $lookup: {
+          from: 'medico',
+          localField: 'medico',
+          foreignField: '_id',
+          as: 'medico'
+        }
+      },
+      {
+        $lookup: {
+          from: 'especialidad',
+          localField: 'medico.especialidades',
+          foreignField: '_id',
+          as: 'especialidad'
+        }
+      },
+      {
+        $unwind: "$especialidad"
+      },
+      {
+        $match: {
+          "especialidad._id": especialidadId
+        }
+      }
+    ]);
+    
+
+    if (tratamientos.length === 0) {
+      return res.status(404).json({ message: 'No existen tratamientos con dosificaciones para el rango de fechas seleccionado' });
+    }
+
+    console.log(tratamientos)
+   
+    // Aceptar la solicitud
+    next();
+  } catch (err) {
+    return res.status(500).json({ 
+      message: 'Error interno del serviddor',
+      errors: err.errors
+    });
+  }
+};
+
+export { 
+    validarRequestMedicamentos, 
+    verificarEspecialidad, 
+    existenTratamientosEnEsaFecha, 
+    existenTratamientosRealizadosPorMedicosConEsaEspecialidad, 
+    existenTratamientosEnEsaFechaConDosificaciones 
+}
---
+++ middlewares/middleware_tratamiento_farmacologico.js
@@ -0,0 +1,140 @@
+import mongoose from 'mongoose'
+import HistoriaClinica from '../models/historia_clinica.js'
+import Diagnostico from '../models/diagnostico.js'
+
+/* Verificar que el usuario haya ingresado dosificaciones */
+const verificarDosificaciones = async (req, res, next) => {
+  const params = req.body;
+
+  if (!params.dosificaciones || !Array.isArray(params.dosificaciones)) {
+      return res.status(400).json({ message: 'El campo "dosificaciones" debe ser un array' });
+  }
+  
+  next();
+};
+
+const existeHistoriaClinica = async (req, res, next) => {
+  const params = req.body;
+
+  try {
+      const historia_clinica = await HistoriaClinica.findOne({ _id: params.historia_clinica })
+
+      if (! historia_clinica) {
+          return res.status(404).json({ message: 'La Historia Clinica no existe en la base de datos' });
+      }
+
+      next();
+  } catch (err) {
+      return res.status(500).json({ 
+          message: 'Error al verificar si existe la Historia Clinica',
+          errors: err.errors
+      });
+  }
+};
+
+const existeDiagnostico = async (req, res, next) => {
+  const params = req.body;
+
+  try {
+      const diagnostico = await Diagnostico.findOne({ _id: params.diagnostico })
+
+      if (! diagnostico) {
+          return res.status(404).json({ message: 'El Diagnostico no existe en la base de datos' });
+      }
+
+      next();
+  } catch (err) {
+      return res.status(500).json({ 
+          message: 'Error en la consulta de diagnóstico',
+          errors: err.errors
+      });
+  }
+};
+
+/* Verifica que el diagnostico pertenezca a una consulta cargada en la Historia Clinica */
+const verificarConsultaDelDiagnostico = async (req, res, next) => {
+  const params = req.body;
+
+  try {
+      const historia_clinica = await HistoriaClinica.findOne({ _id: params.historia_clinica })
+
+      const diagnostico = await Diagnostico.findOne({ _id: params.diagnostico })
+
+      let consultaEncontrada = false;
+
+      historia_clinica.consultas.forEach(consulta => {
+          if (consulta.toString() == diagnostico.consulta.toString())
+              consultaEncontrada = true
+      });
+
+      if (! consultaEncontrada)
+          return res.status(404).json({ message: 'La consulta no se encuentra cargada en la Historia Clinica' });
+
+      next();
+  } catch (err) {
+      return res.status(500).json({ 
+          message: 'Error en la consulta de diagnóstico',
+          errors: err.errors 
+      });
+  }
+};
+
+/* Verifica que el diagnostico del tratamiento este cargado en la Historia Clinica del paciente */
+const verificarDiagnosticoEnHistoriaClinica = async (req, res, next) => {
+  const params = req.body;
+
+  const diagnosticoId = new mongoose.Types.ObjectId(params.diagnostico)
+
+  try {
+      const historia_clinica = await HistoriaClinica.findOne({ _id: params.historia_clinica })
+
+      let diagnosticoEncontrado = false;
+
+      historia_clinica.diagnosticos.forEach(diagnostico => {
+          if (diagnostico.toString() == diagnosticoId.toString())
+              diagnosticoEncontrado = true
+      });
+
+      if (! diagnosticoEncontrado)
+          return res.status(404).json({ error: 'El diagnostico no se encuentra cargado en la Historia Clinica' });
+
+      next();
+  } catch (err) {
+      return res.status(500).json({ 
+          message: 'Error en la consulta de diagnóstico',
+          errors: err.errors 
+      });
+  }
+};
+
+/* Verifica que los diagnosticos pertenecen al mismo medico que solicito el tratamiento */
+const verificarMedicoDelDiagnostico = async (req, res, next) => {
+  const params = req.body;
+  
+  const medicoId = new mongoose.Types.ObjectId(params.medico);
+
+  try {
+      const diagnostico = await Diagnostico.findOne({ _id: params.diagnostico }).populate('consulta')
+
+      if (medicoId.toString() !== diagnostico.consulta.medico.toString()) {
+          return res.status(500).json({ error: 'El médico del diagnóstico no coincide con el del tratamiento' });
+      }
+      
+      // Aceptar la solicitud
+      next();
+  } catch (err) {
+      return res.status(500).json({ 
+          message: 'Error en la consulta de diagnóstico',
+          errors: err.errors 
+      });
+  }
+};
+
+export {
+  verificarDosificaciones,
+  existeHistoriaClinica,
+  existeDiagnostico,
+  verificarConsultaDelDiagnostico,
+  verificarDiagnosticoEnHistoriaClinica,
+  verificarMedicoDelDiagnostico
+}
